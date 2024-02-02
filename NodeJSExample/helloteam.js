ea91384ad7603006ab9e13f12cc45e246ab42db5
OscarCode442 <ukaegbueze442@yahoo.com>
2024-02-02 20:55:42 +0100

    added my name to the repo

--- NodeJSExample/helloteam.js
+++ NodeJSExample/helloteam.js
@@ -1,4 +1,4 @@
-const names = ['Giacomo Draghi','Isaac Maughan', 'Leonardo Mosiah'];
+const names = ['Giacomo Draghi','Isaac Maughan', 'Leonardo Mosiah',"Ukaegbu Japhet Oscar"];
 
 for (const name of names) {
     console.log(`Hello ${name}`);
